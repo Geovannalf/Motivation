3f8bac6899316bd11d5d0140d99c64a64d045a93
Geovannalf <geovannalima0205@gmail.com>
2023-01-18 17:20:04 -0300

    Criação segundo layout/activity

---
+++ app/src/main/java/br/com/motivation/UserActivity.kt
@@ -0,0 +1,31 @@
+package br.com.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import br.com.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(v:View) {
+
+        if ( v.id == R.id.button_save){
+
+        }
+
+    }
+}

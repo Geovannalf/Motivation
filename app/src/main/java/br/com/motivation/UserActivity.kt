423e5facd04d20af4e08d838cf15bcd6c11cdc5f
Geovannalf <geovannalima0205@gmail.com>
2023-01-19 18:15:44 -0300

    SharedPreferences

--- app/src/main/java/br/com/motivation/UserActivity.kt
+++ app/src/main/java/br/com/motivation/UserActivity.kt
@@ -1,8 +1,10 @@
 package br.com.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import br.com.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,11 +23,24 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
     }
 
-    override fun onClick(v:View) {
+    override fun onClick(v: View) {
 
-        if ( v.id == R.id.button_save){
-
+        if (v.id == R.id.button_save) {
+            handleSave()
         }
 
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+
+            SecurityPreferences(this).storeString("USER_NAME",name)
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
+        }
     }
 }
